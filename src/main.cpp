fe4dc77d3008558eb1365ff03e61c3e403e8eb67
kraxarn <me@kraxarn.com>
2021-05-24 22:32:46 +0200

    Testing Raylib stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,20 @@
-#include "clf/clfv2.hpp"
-#include "clf/clfv3.hpp"
-
-#include <iostream>
-#include <filesystem>
+#include "raylib.h"
 
 auto main(int argc, char **argv) -> int
 {
-	std::filesystem::path out("assets/level/v3");
-	for (const auto &dir : std::filesystem::directory_iterator("assets/level/v2"))
-	{
-		const auto &path = dir.path();
-		auto level = clf_v2::parse(path);
-		std::cout << path << " => ";
+	InitWindow(1280, 720, "4mb-platformer");
+	SetTargetFPS(60);
 
-		std::filesystem::path out_path = out / path.stem();
-		clf_v3::save(level, out_path);
-		std::cout << out_path << std::endl;
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(BLACK);
+		{
+			DrawText(":3", 190, 200, 20, RAYWHITE);
+		}
+		EndDrawing();
 	}
 
+	CloseWindow();
 	return 0;
 }
