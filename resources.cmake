d6e5795d287fc2766463ad78f6284bd1ca10305e
kraxarn <me@kraxarn.com>
2021-05-29 12:39:26 +0200

    Embedded music

--- resources.cmake
+++ resources.cmake
@@ -1,8 +1,8 @@
 include(thirdparty/cmrc/CMakeRC.cmake)
 
-# Levels
 cmrc_add_resource_library(res
 	WHENCE assets
+	# Levels
 	assets/level/1-1.clf
 	assets/level/1-2.clf
 	assets/level/1-3.clf
@@ -12,4 +12,10 @@ cmrc_add_resource_library(res
 	assets/level/2-2.clf
 	assets/level/2-3.clf
 	assets/level/2-4.clf
-	assets/level/2-B.clf)
+	assets/level/2-B.clf
+	# Music
+	assets/music/level1.xm
+	assets/music/level2.xm
+	assets/music/menu.xm
+	assets/music/boss.xm
+	assets/music/credits.xm)
