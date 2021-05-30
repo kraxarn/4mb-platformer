31e9b581c51ca30ccfcf784d6fe250dda6a5ba0a
kraxarn <me@kraxarn.com>
2021-05-30 21:30:49 +0200

    Initial image

---
+++ src/engine/image.cpp
@@ -0,0 +1,8 @@
+#include "image.hpp"
+
+ce::image::image(const cmrc::file &file)
+	: Image(LoadImageFromMemory(".png",
+	(const unsigned char *) file.cbegin(),
+	static_cast<int>(file.size())))
+{
+}
---
+++ src/engine/image.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "res.hpp"
+#include "raylib.h"
+
+namespace ce
+{
+	class image: public Image
+	{
+	public:
+		explicit image(const cmrc::file &file);
+	};
+}
