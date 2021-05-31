3575373a084da1403b27f22a3428d2b88d100287
kraxarn <me@kraxarn.com>
2021-05-31 19:53:42 +0200

    Some new texture methods

--- src/engine/texture.cpp
+++ src/engine/texture.cpp
@@ -1,6 +1,47 @@
 #include "texture.hpp"
 
 ce::texture::texture(const ce::image &image)
-	: Texture2D(LoadTextureFromImage(image))
+	: r_texture(LoadTextureFromImage(image))
 {
+}
+
+void ce::texture::draw()
+{
+	DrawTexture(r_texture, x, y, WHITE);
+}
+
+void ce::texture::set_x(int value)
+{
+	x = value;
+}
+
+auto ce::texture::get_x() const -> int
+{
+	return x;
+}
+
+void ce::texture::set_y(int value)
+{
+	y = value;
+}
+
+auto ce::texture::get_y() const -> int
+{
+	return y;
+}
+
+void ce::texture::move(int x_offset, int y_offset)
+{
+	x += x_offset;
+	y += y_offset;
+}
+
+auto ce::texture::get_width() const -> int
+{
+	return r_texture.width;
+}
+
+auto ce::texture::get_height() const -> int
+{
+	return r_texture.height;
 }
--- src/engine/texture.hpp
+++ src/engine/texture.hpp
@@ -5,9 +5,28 @@
 
 namespace ce
 {
-	class texture: public Texture2D
+	class texture
 	{
 	public:
 		explicit texture(const ce::image &image);
+
+		void draw();
+
+		void set_x(int value);
+		auto get_x() const -> int;
+
+		void set_y(int value);
+		auto get_y() const -> int;
+
+		void move(int x_offset, int y_offset);
+
+		auto get_width() const -> int;
+		auto get_height() const -> int;
+
+	private:
+		Texture2D r_texture;
+
+		int x = 0;
+		int y = 0;
 	};
 }
