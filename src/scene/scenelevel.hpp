4d2149a2401cc3e3b68d9079081b709110a2b8d7
kraxarn <me@kraxarn.com>
2021-06-12 11:46:14 +0200

    Use sizes from map

--- src/scene/scenelevel.hpp
+++ src/scene/scenelevel.hpp
@@ -11,6 +11,7 @@
 #include "engine/physics.hpp"
 #include "engine/staticbody.hpp"
 #include "engine/clock.hpp"
+#include "engine/map.hpp"
 #include "level/levelloader.hpp"
 #include "entity/player.hpp"
 
@@ -30,10 +31,8 @@ private:
 	static constexpr float tile_scale = 3.F;
 	static constexpr float tile_size = tileset_size * tile_scale;
 
-	static constexpr int level_tiles_width = 75;
-	static constexpr int level_tiles_height = 25;
-	static constexpr int level_width = level_tiles_width * tile_size;
-	static constexpr int level_height = level_tiles_height * tile_size;
+	static constexpr int level_width = ce::level_tiles_width * tile_size;
+	static constexpr int level_height = ce::level_tiles_height * tile_size;
 
 	static constexpr char spawn_index = 50;
 
