45973f42c3fe9c9b0df585d5f912eb14ccd7b10f
kraxarn <me@kraxarn.com>
2021-06-13 17:08:39 +0200

    Set one way position

--- src/physics/onewaybody.cpp
+++ src/physics/onewaybody.cpp
@@ -1,9 +1,10 @@
 #include "onewaybody.hpp"
 
 phys::one_way_body::one_way_body(cpBody *body, cpShape *shape, const ce::vector2f &position)
-	: static_body(body, shape, position),
+	: static_body(body, shape),
 	normal(0.F, 1.F)
 {
+	set_position(position);
 }
 
 auto phys::one_way_body::get_normal() const -> ce::vector2f
