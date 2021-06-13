bbe30ba506e62749e6fbec0c262727abb73b2aa4
kraxarn <me@kraxarn.com>
2021-06-13 13:06:08 +0200

    Properly reset physics on level reset

--- src/scene/scenelevel.cpp
+++ src/scene/scenelevel.cpp
@@ -42,7 +42,7 @@ void scene_level::render()
 
 void scene_level::load(int index)
 {
-	//physics.reset();
+	physics.clear();
 
 	auto *new_level = level_loader::get(index);
 	if (new_level == nullptr)
