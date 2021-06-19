62029e0abc65101386b90ef04010a16a64804e80
kraxarn <me@kraxarn.com>
2021-06-19 11:31:56 +0200

    Remove all old physics code

--- src/physics/dynamicbody.cpp
+++
@@ -1,8 +0,0 @@
-#include "physics/dynamicbody.hpp"
-
-phys::dynamic_body::dynamic_body(cpBody *body, cpShape *shape)
-	: phys::body(body, shape)
-{
-	set_elasticity(0.F);
-	set_friction(0.9F);
-}
--- src/physics/staticbody.cpp
+++
@@ -1,8 +0,0 @@
-#include "staticbody.hpp"
-
-phys::static_body::static_body(cpBody *body, cpShape *shape)
-	: phys::body(body, shape)
-{
-	set_elasticity(1.F);
-	set_friction(1.F);
-}
