5ec8079455914e43daf8280b6073413036e7c131
kraxarn <me@kraxarn.com>
2021-06-19 13:50:43 +0200

    Add sound effects

--- resources.cmake
+++ resources.cmake
@@ -8,6 +8,13 @@ cmrc_add_resource_library(res
 	assets/music/menu.xm
 	assets/music/boss.xm
 	assets/music/credits.xm
+	# Sound
+	assets/sound/boss.wav
+	assets/sound/coin.wav
+	assets/sound/complete.wav
+	assets/sound/fall.wav
+	assets/sound/gem.wav
+	assets/sound/jump.wav
 	# Fonts
 	assets/font/menu.ttf
 	assets/font/submenu.ttf
