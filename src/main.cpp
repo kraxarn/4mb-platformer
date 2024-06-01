3352f2c4d0b6a84b627d3350521515438b97a784
kraxarn <kraxie@protonmail.com>
2024-06-01 15:44:46 +0200

    Use chirp game instead

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,38 @@
 #include "colors.hpp"
-#include "state.hpp"
-#include "engine/draw.hpp"
-#include "engine/window.hpp"
-#include "enum/scenes.hpp"
 #include "scene/scenelevel.hpp"
+#include "scene/scenemenu.hpp"
 
-#include <chirp/assets.hpp>
-#include <chirp/audio.hpp>
-#include <chirp/clock.hpp>
-#include <chirp/logger.hpp>
+#include <chirp/game.hpp>
 #include <chirp/os.hpp>
+
+class game final: public chirp::game
+{
+public:
+	game()
+	{
+		set_window_title("Tixel - 4MB Platformer");
+		set_window_size({1280, 720});
+		set_target_fps(60);
+		set_clear_color(color::background);
+
+		if (!chirp::os::is_debug())
+		{
+			scenes().push<scene_level>();
+
+			const auto scene = scenes().peek();
+			auto *level = dynamic_cast<scene_level *>(scene.get());
+			level->load(0);
+		}
+		else
+		{
+			scenes().push<scene_menu>();
+		}
+	}
+};
 
 auto main(int /*argc*/, char **/*argv*/) -> int
 {
-	chirp::logger::init();
-
-	ce::window window(1280, 720, "Tixel - 4MB Platformer");
-	chirp::audio audio;
-
-	const chirp::assets assets;
-	ce::window::set_icon(assets.tileset("player")->at(1));
-
-	if (!chirp::os::is_debug())
-	{
-		state::set(scene::menu, assets);
-	}
-	else
-	{
-		state::set(scene::level, assets);
-		dynamic_cast<scene_level *>(state::get().get())->load(0);
-	}
-
-	while (!ce::window::should_close())
-	{
-		state::get()->update(chirp::clock::delta());
-
-		window.begin();
-		ce::draw::clear(color::background);
-		{
-			state::get()->draw();
-		}
-		window.end();
-	}
-
+	game game;
+	game.run();
 	return 0;
 }
