059cd3f25c02d00ff93c115c670ca227cac3000c
kraxarn <kraxie@protonmail.com>
2024-06-24 12:54:46 +0200

    Add basic pause menu

--- src/entity/pause.cpp
+++ src/entity/pause.cpp
@@ -1,14 +1,23 @@
 #include "pause.hpp"
 #include "colors.hpp"
 
+#include <chirp/format.hpp>
 #include <chirp/rectangle.hpp>
+#include <chirp/scene.hpp>
+#include <chirp/scenemanager.hpp>
 #include <chirp/window.hpp>
+
+#include "map.hpp"
+#include "player.hpp"
 
 entity::pause::pause(const chirp::assets &assets, const chirp::window &window)
 	: fnt_title(assets.font("menu", title_size)),
+	fnt_menu(assets.font("submenu", menu_size)),
 	txt_title(fnt_title, "Paused", {0, 0}, title_size, color::text),
+	txt_menu(fnt_menu, "", {0, 0}, menu_size, color::text),
 	window(window)
 {
+	update_position(0);
 }
 
 auto entity::pause::get_paused() const -> bool
@@ -19,9 +28,10 @@ auto entity::pause::get_paused() const -> bool
 void entity::pause::set_paused(bool value)
 {
 	paused = value;
+	update_position(-current_position);
 }
 
-void entity::pause::update(const chirp::scene &/*scene*/, const float /*delta*/)
+void entity::pause::update(const chirp::scene &scene, const float /*delta*/)
 {
 	if (!paused)
 	{
@@ -35,6 +45,48 @@ void entity::pause::update(const chirp::scene &/*scene*/, const float /*delta*/)
 		window_size.x() / 2 - text_size.x() / 2,
 		window_size.y() / 2 - text_size.y() / 2,
 	});
+
+	txt_menu.set_position({
+		txt_title.get_position().x(),
+		txt_title.get_position().y() + text_size.y() + 16,
+	});
+
+	if (keymap.is_pressed("up"))
+	{
+		update_position(-1);
+	}
+	else if (keymap.is_pressed("down"))
+	{
+		update_position(1);
+	}
+	else if (keymap.is_pressed("enter"))
+	{
+		if (current_position == 0)
+		{
+			// Resume
+			set_paused(false);
+		}
+		else if (current_position == 1)
+		{
+			// Restart level
+			const auto &player = scene.find<::entity::player>("cam_main/spr_player");
+			const auto &map = scene.find<::entity::map>("cam_main/map_main");
+
+			if (player && map)
+			{
+				const auto safe_spawn = map->get_level()->get_safe_spawn();
+				player->set_velocity({0, 0});
+				player->set_position(safe_spawn);
+			}
+
+			set_paused(false);
+		}
+		else if (current_position == 2)
+		{
+			// Back to menu
+			scene.scenes().queue_pop();
+		}
+	}
 }
 
 void entity::pause::draw() const
@@ -53,4 +105,29 @@ void entity::pause::draw() const
 	chirp::draw_filled(background, {0x0, 0x0, 0x0, 0x7f});
 
 	txt_title.draw();
+	txt_menu.draw();
+}
+
+void entity::pause::update_position(const int mod)
+{
+	current_position += mod;
+
+	constexpr auto min = 0;
+	constexpr auto max = 2;
+
+	if (current_position < min)
+	{
+		current_position = max;
+	}
+	else if (current_position > max)
+	{
+		current_position = min;
+	}
+
+	const auto text = chirp::format("{}Resume\n{}Restart level\n{}Back to menu",
+		current_position == 0 ? "> " : "",
+		current_position == 1 ? "> " : "",
+		current_position == 2 ? "> " : "");
+
+	txt_menu.set_text(text);
 }
--- src/entity/pause.hpp
+++ src/entity/pause.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include "keymap.hpp"
+
+#include <chirp/asset.hpp>
 #include <chirp/assets.hpp>
 #include <chirp/entity.hpp>
+#include <chirp/font.hpp>
 #include <chirp/text.hpp>
 #include <chirp/window.hpp>
 
@@ -19,12 +23,21 @@ namespace entity
 		void set_paused(bool value);
 
 	private:
-		static constexpr int title_size = 42;
+		static constexpr auto title_size = 42;
+		static constexpr auto menu_size = 32;
 
 		bool paused = false;
+		int current_position = 0;
 
 		chirp::asset<chirp::font> fnt_title;
+		chirp::asset<chirp::font> fnt_menu;
+
 		chirp::text txt_title;
+		chirp::text txt_menu;
+
 		const chirp::window &window;
+		::keymap keymap;
+
+		void update_position(int mod);
 	};
 }
